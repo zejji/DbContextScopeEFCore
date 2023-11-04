2eee17d4a15117ce58ec4a2bf863481f40995552
zejji <jezhl@yahoo.co.uk>
2023-11-04 16:07:11 +0000

    Add ServiceProviderDbContextFactory

---
+++ DbContextScope/Implementations/ServiceProviderDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Zejji.Entity;
+
+/// <summary>
+/// A factory to create <see cref="DbContext"/> instances
+/// using an <see cref="IServiceProvider"/> instance.
+/// </summary>
+/// <remarks>
+/// It should be registered in a dependency injection container as a singleton.
+/// </remarks>
+public class ServiceProviderDbContextFactory(IServiceProvider serviceProvider)
+    : IDbContextFactory
+{
+    public TDbContext CreateDbContext<TDbContext>()
+        where TDbContext : DbContext
+    {
+        return (TDbContext)ActivatorUtilities.CreateInstance(serviceProvider, typeof(TDbContext));
+    }
+}
